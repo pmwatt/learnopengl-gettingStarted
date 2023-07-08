1d7f14bb1021c4e477bbe000344fb4ff8b20288c
pmwatt <prachnachai.mek@student.mahidol.edu>
2023-07-08 17:16:49 +0700

    +rainbow triangle

--- gettingStarted/application.cpp
+++ gettingStarted/application.cpp
@@ -65,10 +65,13 @@ int main()
 
     // vertex shader source
     const char* vertexShaderSource = "#version 330 core\n"
-        "layout (location = 0) in vec3 aPos;\n"
+        "layout (location = 0) in vec3 aPos;\n" // attrib position 0
+        "layout (location = 1) in vec3 aColor;\n" // attrib position 1
+        "out vec3 ourColor;\n" // output to pixel shader
         "void main()\n"
         "{\n"
         " gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n" // output
+        "ourColor = aColor;\n" // set ourColor to input color from the vertex data
         "}\0"; // null terminated
 
     // create, bind, compile vertex shader
@@ -80,10 +83,10 @@ int main()
     // fragment/pixel shader source
     const char* fragmentShaderSource = "#version 330 core\n"
         "out vec4 FragColor;\n"
-        "uniform vec4 ourColor;\n"
+        "in vec3 ourColor;\n" // from vertex shader
         "void main()\n"
         "{\n"
-        "FragColor = ourColor;\n"
+        "FragColor = vec4(ourColor, 1.0);\n" // casting
         "}\0"; // null terminated
 
     // create and bind fragment shader
@@ -121,9 +124,10 @@ int main()
 
     // vertices
     float vertices[] = {
-        -0.5f,  -0.5f,  0.0f,
-        0.5f,   -0.5f,  0.0f,
-        0.0f,   0.5f,   0.0f
+        // positions            // colors
+        0.5f, -0.5f, 0.0f,      1.0f, 0.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,     0.0f, 1.0f, 0.0f, // bottom left
+        0.0f, 0.5f, 0.0f,       0.0f, 0.0f, 1.0f // top
     };
 
     // generate vertex buffer array and bind (VAO)
@@ -138,8 +142,13 @@ int main()
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
     // specify how to read the vertices / vertex attrib config
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+    // position attribute
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
+
+    // color attribute
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float))); // cast to (void*)?
+    glEnableVertexAttribArray(1);
 
     /////////////////////////////////////////////////////////////
 
